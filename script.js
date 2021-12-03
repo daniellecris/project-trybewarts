fc1b943a278cec3065317ce0f2f7b6c0944ebde8
Yara <yara.apas@gmail.com>
2021-12-03 10:47:45 -0300

    Requisito 3 completo

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//const email = document.querySelector('')
+const email = document.querySelector('#email');
+const senha = document.querySelector('#senha');
+
+function verificarDados() {
+  if ((email.value === 'tryber@teste.com') && (senha.value === '123456')) {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+}
+
+const confirmar = document.getElementById('botao');
+confirmar.addEventListener('click', verificarDados);
